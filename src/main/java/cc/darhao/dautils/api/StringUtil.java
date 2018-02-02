b3fa9fce54a1c1dd16c45a263d7fa71ad85f88fa
darhao <darhao@qq.com>
2018-02-02 11:38:57 +0800

    增加StringUtil的功能
    Signed-off-by: darhao <darhao@qq.com>

--- src/main/java/cc/darhao/dautils/api/StringUtil.java
+++ src/main/java/cc/darhao/dautils/api/StringUtil.java
@@ -1,5 +1,8 @@
 package cc.darhao.dautils.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 字符串工具
  * <br>
@@ -12,7 +15,6 @@ public class StringUtil {
 	 * 把一个字符串变成指定长度，不够则会在高位补零
 	 * @param string 被处理的数字串
 	 * @param length 需要变成多长
-	 * @return
 	 */
 	public static String fixLength(String string, int length) throws NumberFormatException{
 		if(string.length() >= length ) {
@@ -25,5 +27,38 @@ public class StringUtil {
 		}
 		return string;
 	}
+	
+	
+	/**
+	 * 把一个字符串里面的空格全部去掉
+	 * @param string 被处理的数字串
+	 */
+	public static String press(String string) {
+		StringBuffer sb = new StringBuffer();
+		String[] strings = string.split(" ");
+		for (String string2 : strings) {
+			sb.append(string2);
+		}
+		return sb.toString();
+	}
+	
+	
+	/**
+	 * 把一个字符串按一定长度用空格分开
+	 * @param string 被处理的数字串
+	 */
+	public static String stretch(String string, int length) {
+		List<String> strings = new ArrayList<String>();
+		for (int i = 0; i <= string.length() - length; i+=length) {
+			strings.add(string.substring(i, i+length));
+		}
+		StringBuffer sb = new StringBuffer();
+		for (String s: strings) {
+			sb.append(s);
+			sb.append(" ");
+		}
+		return sb.toString().trim();
+	}
+	
 	
 }
